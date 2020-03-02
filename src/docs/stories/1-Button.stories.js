12e7c756a7e10daf4e5abb958ff1adbb9a8878e5
Matheus Alexandre <matheus.alexandre@homehero.com.br>
2020-03-02 17:39:13 -0300

    [hotfix] - Solved issue due storybook that was not rendering components

--- src/docs/stories/1-Button.stories.js
+++ src/docs/stories/1-Button.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import Button from '../orb/components/atom/button/Button';
+import Button from '../../components/atom/button/Button';
 
 export default {
   title: 'Button',
